9c738eed4c43860096cbabfe14b04353b571418b
dscloudit <68459339+dscloudit@users.noreply.github.com>
2020-09-17 13:53:56 -0700

    Update Master.ps1

--- Master.ps1
+++ Master.ps1
@@ -1,13 +1,13 @@
-New-Item -path "c:\" -Name "Support" -ItemType "Directory"
 $RootDirectory = "c:\support\"
-#Set policy to remote signed so we can run scripts locally
-Set-ExecutionPolicy RemoteSigned
 
-#Download Master Script
 $WebClient = New-Object -TypeName System.Net.Webclient
 $URIBASE = "https://raw.githubusercontent.com/dscloudit/CITPublic/master/"
-$TargetPath = $RootDirectory + "Master.ps1"
-$WebClient.DownloadFile($uri, $TargetPath)
 
-#Execute Master Script
-Invoke-Command -FilePath $RootDirectory + "Master.ps1"
+#Download scripts for initial setup
+$TargetPath = $RootDirectory + "InitialConnection.ps1"
+$WebClient.DownloadFile($uri, $TargetPath)
+$TargetPath = $RootDirectory + "InitialConnection.xml"
+$WebClient.DownloadFile($uri, $TargetPath)
+#Register first script as a scheduled task
+
+Register-ScheduledTask -Xml (get-content $RootDirectory + "initialconnection.xml" | Out-String) -TaskName "Initial Connection"
